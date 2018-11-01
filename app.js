50325198ee1054703c76a59e466fd3be79affd9a
Yura Shalya <stallker9696@gmail.com>
2018-11-01 22:55:12 +0200

    creating authorization routes

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require ('express');
+const authRoutes = require ('./routes/auth')
+const app = express()
+
+
+app.use('/api/auth', authRoutes)
+
+module.exports = app;
---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+module.exports.login = function(req,res) {
+    res.status(200).json({
+        login: 'form controller'
+    })
+}
+
+
+module.exports.register = function(req,res) {
+    res.status(200).json({
+        register: 'form controller'
+    })
+}
