8331d61cf6b2eb2d15957f010b722d566061ac9a
Yura Shalya <stallker9696@gmail.com>
2018-11-02 00:30:17 +0200

    Creation of all routes in the project

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,11 @@ const express = require('express')
 const controller = require('../controllers/auth') 
 const router = express.Router()
 
-router.get('/login', controller.login)
-router.get('/register', controller.register)
+
+// localhost:5000/api/auth/login
+router.post('/login', controller.login)
+//localhost:5000/api/auth/register
+router.post('/register', controller.register)
 
  
 module.exports = router
